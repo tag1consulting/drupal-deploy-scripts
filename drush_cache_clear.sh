05f830bb083e4812aeab56c4c2bb42ab73913a71
Jeff Sheltren <jeff@tag1consulting.com>
2015-04-02 12:51:14 -0700

    Update cache-clear script to also work with D8

--- drush_cache_clear.sh
+++ drush_cache_clear.sh
@@ -3,19 +3,27 @@
 # Run a 'drush cc all' for a specified site.
 # This is a simple wrapper script that can be added to sudo config
 # so that the deploy user can run a cache clear as the Apache/web user.
+# For d8 sites, this will call 'cache-rebuild' if you pass the -r flag.
 
 usage() {
-  echo "usage: $0 [-d @drush_alias]"
+  echo "usage: $0 [-d @drush_alias] [-r]"
   echo "    -d          Specify drush alias (include leading @)"
+  echo "    -r          Call cache-rebuild instead of cache-clear (use this for D8 sites)"
   echo ""
   echo "Drush alias is required."
   exit 1
 }
 
-while getopts "d:" opt; do
+# Default drush command is 'cc all'. Override if -r is specified.
+COMMAND='cc all'
+
+while getopts "d:r" opt; do
   case $opt in
     d)
       DRUSH_ALIAS=$OPTARG
+      ;;
+    r)
+      COMMAND=cr
       ;;
     *)
       usage
@@ -32,4 +40,4 @@ HOSTNAME=$(uname -n)
 echo "On host ${HOSTNAME}"
 echo "Going to clear all caches on site: $DRUSH_ALIAS"
 
-/usr/bin/drush $DRUSH_ALIAS cc all || { echo "Drush command exited with an error."; exit 1; }
+/usr/bin/drush $DRUSH_ALIAS $COMMAND || { echo "Drush command exited with an error."; exit 1; }
