79216c8f0521cac88cfddfe7aeda0a5d88139c8a
Jeff Sheltren <jeff@tag1consulting.com>
2015-04-03 08:59:44 -0700

    Add config dir symlink to symlink script (currently untested)

--- adjust_live_symlink.sh
+++ adjust_live_symlink.sh
@@ -2,6 +2,7 @@
 #
 # Update the webroot symlink to point at a new release tag.
 # Use with care, this updates the LIVE website symlink.
+# If passed the -c flag, it will also update symlinks to the Drupal config directory.
 
 # Change this if deploy_settings file is in a different location.
 DEPLOY_SETTINGS=/usr/local/deploy/deploy_settings
@@ -10,13 +11,19 @@ usage() {
   echo "usage: $0 [-d @drush_alias] [-t git tag]"
   echo "    -d          Specify drush alias (include leading @)"
   echo "    -t          Specify git tag to link to"
+  echo "    -c          Also create symlink for the Drupal config directory"
   echo ""
-  echo "Both arguments are required."
+  echo "-d and -t arguments are required."
   exit 1
 }
 
-while getopts "d:t:" opt; do
+CONFIG_SYMLINK=false
+
+while getopts "d:t:c" opt; do
   case $opt in
+    c)
+      CONFIG_SYMLINK=true
+      ;;
     d)
       DRUSH_ALIAS=$OPTARG
       ;;
@@ -70,3 +77,24 @@ ln -s $TARGET_DIR $WEBROOT_SYMLINK || { echo "Error creating new symlink."; exit
 
 echo "Outputting ls of new symlink:"
 ls -l $WEBROOT_SYMLINK
+
+if [ "$CONFIG_SYMLINK" == "true" ]
+then
+  echo "Updating symlink to Drupal config directory."
+  CONFIG_DIR=$($DRUSH_CMD $DRUSH_ALIAS config-dir 2>/dev/null)
+  if [ -z "${CONFIG_DIR}" ] || [ ! -d ${RELEASE_DIR}/${GIT_TAG}/${CONFIG_DIR} ]
+  then
+    echo "Config directory (${RELEASE_DIR}/${GIT_TAG}/${CONFIG_DIR}}) doesn't exist. Ensure ['shell-aliases']['config-dir'] is set for this drush alias."
+    exit 1
+  fi
+  # Error out if our config symlink already exists but isn't a symlink.
+  if [ -f ${BASEDIR}/${CONFIG_DIR} ] && [ ! -L ${BASEDIR}/${CONFIG_DIR} ]
+  then
+    echo "${BASEDIR}/${CONFIG_DIR} exists and is not a symlink, refusing to delete it."
+    exit 1
+  fi
+  rm -f ${BASEDIR}/${CONFIG_DIR}
+  ln -s ${RELEASE_DIR}/${GIT_TAG}/${CONFIG_DIR} ${BASEDIR}/${CONFIG_DIR}
+  echo "Outputting ls of new config symlink:"
+  ls -l ${BASEDIR}/${CONFIG_DIR}
+fi
